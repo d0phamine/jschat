1c40398fdbfb4f4b8385f19b3eaaf0e4a0397f8e
d0phamine <sizow123@gmail.com>
2019-11-12 22:16:54 +0300

    icons and am validation

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -3,23 +3,43 @@ import PropTypes from 'prop-types';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import ruLocale  from 'date-fns/locale/ru'
 import classNames from 'classnames';
-import checkedSvg from 'assets/img/checked.svg';
+import readedSvg from 'assets/img/readed.svg';
+import noReadedSvg from 'assets/img/noreaded.svg';
 
 
 import './Message.scss';
 
-const Message = ({ avatar, text, date, user, isMe }) =>(
+
+const Message = ({ avatar, text, date, user, isMe, isReaded, attachments }) =>(
     <div className={classNames('message', {'message  isme': isMe})}>
-        
         <div className="message__content">
-            <img src={checkedSvg} alt="Checked icon"></img>
+            {isMe && isReaded ? (
+                <img 
+                className="message__icon-readed" 
+                src={readedSvg} 
+                alt="Readed icon" />
+            ) : (
+                <img 
+                className="message__icon-readed message__icon-readed--no" 
+                src={noReadedSvg} 
+                alt="No Readed icon" />
+            )}
             <div className="message__avatar">
-                <img src={avatar} alt={`Avatar ${user.fullname}`}/>
+                    <img src={avatar} alt={`Avatar ${user.fullname}`}/>
             </div>
-            <div className="message__bubble">
-                <p className="message__text" >{ text }</p>
-            </div>
-            <span className="message__date">{ formatDistanceToNow( new Date(2019, 10, 10, 22), {addSuffix: true, locale: ruLocale} ) }</span>
+            <div className="message__info">
+                <div className="message__bubble">
+                    <p className="message__text" >{ text }</p>
+                </div>
+                <div className="message__attachments">
+                    {attachments && attachments.map(item => (
+                        <div className="message__attachments-item">
+                            <img src={item.url} alt={item.filename} />
+                        </div>
+                    ))}
+                </div>
+                <span className="message__date">{ formatDistanceToNow( new Date(2019, 10, 10, 22), {addSuffix: true, locale: ruLocale} ) }</span>
+            </div>   
         </div>
     </div>
 );
@@ -32,7 +52,8 @@ Message.propTypes = {
     avatar: PropTypes.string,
     text: PropTypes.string,
     date: PropTypes.string,
-    user: PropTypes.object
+    user: PropTypes.object,
+    attachments: PropTypes.array,
 };
 
 export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,6 +12,21 @@ const Home = () => (
         avatar="https://sun9-8.userapi.com/c854528/v854528097/123e96/N4Gg333hyZ4.jpg?ava=1" 
         text="Мы тут недавно войска Ариовиста разбили, чуваки хотели закрепиться на галльских землях, лол 🌝" 
         date="Sun Nov 10 2019 21:55:32 "
+        attachments={[
+            {
+                filename: 'image.jpg',
+                url: 'https://source.unsplash.com/100x100/?random-1&nature,water',
+            },
+            {
+                filename: 'image.jpg',
+                url: 'https://source.unsplash.com/100x100/?random-2&nature,water',
+            },
+            {
+                filename: 'image.jpg',
+                url: 'https://source.unsplash.com/100x100/?random-3&nature,water',
+            },
+        ]}
+        
         />
         <Message 
         avatar="https://sun9-24.userapi.com/c854220/v854220647/155f29/oI-XOS1gijU.jpg?ava=1" 
@@ -19,6 +34,7 @@ const Home = () => (
         date="Sun Nov 10 2019 21:55:32 "
         IsMe={true}
         IsReaded={true}
+        
         />
     </section>
 );
